475eee42e2cf1030691ec2c12d1af330fd69f482
unknown <dj.zaragoza@icloud.com>
2020-06-11 18:37:22 -0700

    completed moving zeroes file and test successful

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,8 +2,22 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def moving_zeroes(arr):
     # Your code here
+    # Objective --
+    # shift zeroes left of the array then return the altered array
+    # if no zeroes, print array as is
+
+    moved = 0
+
+    for x in range(len(arr)):
+        if arr[x] != 0 and arr[moved] == 0:
+            arr[x], arr[moved] = arr[moved], arr[x]
+        if arr[moved] != 0:
+            moved += 1
+    return arr
 
     pass
 
